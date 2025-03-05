bef44999f538df9d481cbd571ed519bed213d96a
Rockey C.R <rockeyrai234@gmail.com>
2025-03-05 12:27:34 +0545

    making some change to the project detail

--- my/src/components/canvas/index.js
+++ my/src/components/canvas/index.js
@@ -2,6 +2,5 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
-import RocketCanvas from "./SpaceRocket";
 
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas,RocketCanvas };
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
